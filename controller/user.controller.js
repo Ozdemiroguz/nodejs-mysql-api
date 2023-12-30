7cd17e98d6699ff3dbdc8ea5e8d373d506767666
Oguzhan <ozdemiroguzhan55@gmail.com>
2023-12-31 01:18:36 +0300

    Update user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,78 +1,78 @@
-const pool = require("../database/index")
-const postsController = {
-    getAll: async (req, res) => {
-        try {
-            const [rows, fields] = await pool.query("select * from posts")
-            res.json({
-                data: rows
-            })
-        } catch (error) {
-            console.log(error)
-            res.json({
-                status: "error"
-            })
-        }
-    },
-    getById: async (req, res) => {
-        try {
-            const { id } = req.params
-            const [rows, fields] = await pool.query("select * from posts where id = ?", [id])
-            res.json({
-                data: rows
-            })
-        } catch (error) {
-            console.log(error)
-            res.json({
-                status: "error"
-            })
-        }
-    },
-    create: async (req, res) => {
-        try {
-            const { title, content } = req.body
-            const sql = "insert into posts (title, content) values (?, ?)"
-            const [rows, fields] = await pool.query(sql, [title, content])
-            res.json({
-                data: rows
-            })
-        } catch (error) {
-            console.log(error)
-            res.json({
-                status: "error"
-            })
-        }
-    },
-    update: async (req, res) => {
-        try {
-            const { title, content } = req.body
-            const { id } = req.params
-            const sql = "update posts set title = ?, content = ? where id = ?"
-            const [rows, fields] = await pool.query(sql, [title, content, id])
-            res.json({
-                data: rows
-            })
-        } catch (error) {
-            console.log(error)
-            res.json({
-                status: "error"
-            })
-        }
-    }, 
-    delete: async (req, res) => {
-        try {
-            const { id } = req.params
-            const [rows, fields] = await pool.query("delete from posts where id = ?", [id])
-            res.json({
-                data: rows
-            })
-        } catch (error) {
-            console.log(error)
-            res.json({
-                status: "error"
-            })
-        }
-    }
+const pool = require("../database/index");
 
-}
+const smartHouseController = {
+    getAllUsers: async (req, res) => {
+        try {
+            const [rows, fields] = await pool.query("SELECT * FROM User");
+            res.json({
+                data: rows
+            });
+        } catch (error) {
+            console.log(error);
+            res.json({
+                status: "error"
+            });
+        }
+    },
+    getUserById: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const [rows, fields] = await pool.query("SELECT * FROM User WHERE UserID = ?", [id]);
+            res.json({
+                data: rows
+            });
+        } catch (error) {
+            console.log(error);
+            res.json({
+                status: "error"
+            });
+        }
+    },
+    createUser: async (req, res) => {
+        try {
+            const { mail, name, surname, password, phone, numberOfRooms, active } = req.body;
+            const sql = "INSERT INTO User (Mail, Name, Surname, Password, Phone, NumberOfRooms, Active) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            const [rows, fields] = await pool.query(sql, [mail, name, surname, password, phone, numberOfRooms, active]);
+            res.json({
+                data: rows
+            });
+        } catch (error) {
+            console.log(error);
+            res.json({
+                status: "error"
+            });
+        }
+    },
+    updateUser: async (req, res) => {
+        try {
+            const { mail, name, surname, password, phone, numberOfRooms, active } = req.body;
+            const { id } = req.params;
+            const sql = "UPDATE User SET Mail = ?, Name = ?, Surname = ?, Password = ?, Phone = ?, NumberOfRooms = ?, Active = ? WHERE UserID = ?";
+            const [rows, fields] = await pool.query(sql, [mail, name, surname, password, phone, numberOfRooms, active, id]);
+            res.json({
+                data: rows
+            });
+        } catch (error) {
+            console.log(error);
+            res.json({
+                status: "error"
+            });
+        }
+    },
+    deleteUser: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const [rows, fields] = await pool.query("DELETE FROM User WHERE UserID = ?", [id]);
+            res.json({
+                data: rows
+            });
+        } catch (error) {
+            console.log(error);
+            res.json({
+                status: "error"
+            });
+        }
+    },
+};
 
-module.exports = postsController
+module.exports = smartHouseController;
