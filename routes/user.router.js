cf9ed4cc4f67f69e6e03ffc22caa1d92220a9e7b
Oguzhan <ozdemiroguzhan55@gmail.com>
2023-12-31 01:19:15 +0300

    Update user.router.js

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const router = express.Router()
 
-const postsController = require("../controller/user.controller")
+const userController = require("../controller/user.controller")
 
-router.get("/", postsController.getAll)
-router.get("/:id", postsController.getById)
-router.post("/", postsController.create)
-router.put("/:id", postsController.update)
-router.delete("/:id", postsController.delete)
+router.get("/", userController.getAll)
+router.get("/:id", userController.getById)
+router.post("/", userController.create)
+router.put("/:id", userController.update)
+router.delete("/:id", userController.delete)
 
 module.exports = router
