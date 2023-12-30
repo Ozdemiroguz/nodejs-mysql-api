9ba61a7baa230c02a223921134ec29660c1b638c
Oğuzhan Özdemir <ozdemiroguzhan55@gmail.com>
2023-12-31 00:20:53 +0300

    user router added

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express()
 
 require('dotenv').config()
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 const postsRouter = require('./routes/posts.router')
 const authRouter = require('./routes/auth.router')
 
-app.use("/api/v1/posts", postsRouter)
+app.use("/api/v1/user", userRouter)
 app.use("/api/v1/auth", authRouter)
 
 const PORT = process.env.PORT || 5000
--- routes/user.router.js
+++ routes/user.router.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const postsController = require("../controller/posts.controller")
+const postsController = require("../controller/user.controller")
 
 router.get("/", postsController.getAll)
 router.get("/:id", postsController.getById)
