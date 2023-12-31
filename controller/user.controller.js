63548731997bd0986d516c33b8de852f7a378d21
Oğuzhan Özdemir <ozdemiroguzhan55@gmail.com>
2024-01-01 00:27:17 +0300

    user controller update

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -31,8 +31,12 @@ const userController = {
     createUser: async (req, res) => {
         try {
             const { mail, name, surname, password, phone, numberOfRooms, active } = req.body;
+
+            // Şifreyi hash'le
+            const hashedPassword = await bcrypt.hash(password, 10);
+
             const sql = "INSERT INTO User (Mail, Name, Surname, Password, Phone, NumberOfRooms, Active) VALUES (?, ?, ?, ?, ?, ?, ?)";
-            const [rows, fields] = await pool.query(sql, [mail, name, surname, password, phone, numberOfRooms, active]);
+            const [rows, fields] = await pool.query(sql, [mail, name, surname, hashedPassword, phone, numberOfRooms, active]);
             res.json({
                 data: rows
             });
@@ -43,12 +47,31 @@ const userController = {
             });
         }
     },
+    changePassword: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const { newPassword } = req.body;
+
+            // Yeni şifreyi hash'le
+            const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+
+            // Yeni şifreyi veritabanında güncelle
+            const updateSql = "UPDATE User SET Password = ? WHERE UserID = ?";
+            const [updateRows, updateFields] = await pool.query(updateSql, [hashedNewPassword, id]);
+
+            res.json({ status: "success", message: "Şifre başarıyla güncellendi." });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ status: "error", message: "İşlem sırasında bir hata oluştu." });
+        }
+    },
     updateUser: async (req, res) => {
         try {
-            const { mail, name, surname, password, phone, numberOfRooms, active } = req.body;
+            const { mail, name, surname, phone, numberOfRooms, active } = req.body;
             const { id } = req.params;
-            const sql = "UPDATE User SET Mail = ?, Name = ?, Surname = ?, Password = ?, Phone = ?, NumberOfRooms = ?, Active = ? WHERE UserID = ?";
-            const [rows, fields] = await pool.query(sql, [mail, name, surname, password, phone, numberOfRooms, active, id]);
+
+            const sql = "UPDATE User SET Mail = ?, Name = ?, Surname = ?, Phone = ?, NumberOfRooms = ?, Active = ? WHERE UserID = ?";
+            const [rows, fields] = await pool.query(sql, [mail, name, surname, phone, numberOfRooms, active, id]);
             res.json({
                 data: rows
             });
--- routes/user.router.js
+++ routes/user.router.js
@@ -6,6 +6,7 @@ const userController = require("../controller/user.controller");
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUserById);
 router.post("/", userController.createUser);
+router.put("/:id", userController.changePassword);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
 
