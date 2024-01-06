7a2d6b71638b9c5fcb923cdb1d36b9fe9d8e67b0
Oğuzhan Özdemir <ozdemiroguzhan55@gmail.com>
2024-01-06 09:35:41 +0300

    temphum deleted

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(express.json())
 const userRouter = require('./routes/user.router')
 const authRouter = require('./routes/auth.router')
 const roomRouter = require('./routes/room.router'); // Import the room router
-const tempHumRouter = require('./routes/temphum.router');// Import the temphum router
 const addSensorRouter = require('./routes/addsensor.router');// Import the addSensor router
 const getSensorRouter = require('./routes/getsensor.router');// Import the getSensor router
 const authMiddleware = require("./middleware/authMiddleware");
@@ -19,7 +18,6 @@ const authMiddleware = require("./middleware/authMiddleware");
 app.use("/api/v1/user", userRouter)// Use the user router
 app.use("/api/v1/auth", authRouter)// Use the auth router
 app.use("/api/v1/room", roomRouter); // Use the room router
-app.use("/api/v1/temp_hum", tempHumRouter);// Use the temphum router
 app.use("/api/v1/addSensor", addSensorRouter);// Use the addSensor router
 app.use("/api/v1/getSensor", getSensorRouter);// Use the getSensor router
 app.use(authMiddleware);// Use the authMiddleware
