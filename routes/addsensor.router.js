d9e1076a12e977d6a905f77a36ffcb930be0cf3d
Oğuzhan Özdemir <ozdemiroguzhan55@gmail.com>
2024-01-06 16:57:33 +0300

    .

--- routes/addsensor.router.js
+++ routes/addsensor.router.js
@@ -2,9 +2,10 @@ const express = require("express")
 const router = express.Router()
 
 const addSensorController = require("../controller/addsensor.controller")
+const authMiddleware = require("../middleware/authMiddleware")
 
 router.post("/addTemperatureHumidity", addSensorController.addTemperatureHumidity)
-router.post("/addGas", addSensorController.addGas)
+router.post("/addGas", authMiddleware, addSensorController.addGas)
 router.post("/addFire", addSensorController.addFire)
 router.post("/addMovement", addSensorController.addMovement)
 router.post("/addPotHumidity", addSensorController.addPotHumidity)
