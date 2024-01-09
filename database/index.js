a7b96739f4bbbd055536aaa104f4ba24e03189ed
Oguzhan <ozdemiroguzhan55@gmail.com>
2024-01-09 09:05:11 +0300

    Update index.js

--- database/index.js
+++ database/index.js
@@ -5,6 +5,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USERNAME, 
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DBNAME,
+    port: 21716,
     waitForConnections: true,
     connectionLimit: 15,
     queueLimit: 0
